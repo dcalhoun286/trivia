488992aaa0912b5fac0761935858e9c7323f4a58
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-01-12 19:16:08 -0800

    added cb for next question button

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ class App extends Component {
       .then(data => this.setState({ question: data.results[0] })) 
   }
 
+  handleNextQuestion = () => {
+    fetch(TRIVIA_API)
+      .then(res => res.json())
+      .then(data => this.setState({ question: data.results[0] })) 
+  }
+
   render() {
     return (
       <div className='container l:w-50 p-5'>
@@ -33,7 +39,7 @@ class App extends Component {
           {this.state.question && <Question question={this.state.question} />}
         </div>
         <div>
-          <button type='button' className='btn btn-primary'>Next Question</button>
+          <button type='button' className='btn btn-primary' onClick={this.handleNextQuestion}>Next Question</button>
         </div>
       </div>
     );
